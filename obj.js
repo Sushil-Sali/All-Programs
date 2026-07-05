df5fda98f57dddd8f0d459f75bf2b688d9ba8438
Sushil-Sali <sushilsali65@gmail.com>
2026-07-05 19:41:34 +0530

    Inital upadate

--- obj.js
+++ obj.js
@@ -13,6 +13,7 @@ for (let i in obj) {
 // for (let i = 0; i < key.length; i++) {
 //   console.log(key[i], obj[key[i]]);
 // }
-
+console.log(obj);
 console.log(Object.keys(obj));
 console.log(Object.values(obj));
+console.log(Object.entries(obj));
