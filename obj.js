758b427e2e33db74ec3ddc76630284e88deb72d8
Sushil-Sali <sushilsali65@gmail.com>
2026-07-07 20:29:49 +0530

    upadate with new stpes

--- obj.js
+++ obj.js
@@ -1,22 +1,22 @@
-let obj = {
-  name: "Joe",
-  age: 22,
-  city: "Pune",
-  state: "Maharashtra",
-};
+// let obj = {
+//   name: "Joe",
+//   age: 22,
+//   city: "Pune",
+//   state: "Maharashtra",
+// };
 
-for (let i in obj) {
-  console.log(`${i} : ${obj[i]}`);
-}
-
-// let key = Object.keys(obj);
-// for (let i = 0; i < key.length; i++) {
-//   console.log(key[i], obj[key[i]]);
+// for (let i in obj) {
+//   console.log(`${i} : ${obj[i]}`);
 // }
-console.log(obj);
-console.log(Object.keys(obj));
-console.log(Object.values(obj));
-console.log(Object.entries(obj));
+
+// // let key = Object.keys(obj);
+// // for (let i = 0; i < key.length; i++) {
+// //   console.log(key[i], obj[key[i]]);
+// // }
+// console.log(obj);
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
 
 let student = {
   name: "Rahul",
@@ -26,4 +26,13 @@ let student = {
   marks: [70, 80, 90, 92],
 };
 
-console.log(student);
+console.log(Object.keys(student));
+console.log(Object.values(student));
+console.log(Object.entries(student));
+
+// console.log(student.marks[0]);
+// console.log(student.marks[1]);
+
+for (let i = 0; i < student.marks.length; i++) {
+  console.log(student.marks[i]);
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log(student.marks[0]);
+// console.log(student.marks[1]);
