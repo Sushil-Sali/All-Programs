a3a63fcfb43cdd28c9758d69127b93799703ea96
Sushil-Sali <sushilsali65@gmail.com>
2026-07-10 13:23:40 +0530

    upadate with new stpes

--- obj.js
+++ obj.js
@@ -37,18 +37,32 @@
 //   console.log(student.marks[i]);
 // }
 
-let student = [
-  { name: "sushil", marks: 87 },
-  { name: "rahul", marks: 90 },
-  { name: "Yogesh", marks: 88 },
-  { name: "Rushi", marks: 95 },
-  { name: "Rushabh", marks: 80 },
-];
+// let student = [
+//   { name: "sushil", marks: 87 },
+//   { name: "rahul", marks: 90 },
+//   { name: "Yogesh", marks: 88 },
+//   { name: "Rushi", marks: 95 },
+//   { name: "Rushabh", marks: 80 },
+// ];
 
-// console.log(student[0]);
-// console.log(student[1].marks);
-for (let i = 0; i < student.length; i++) {
-  if (student[i].marks > 90) {
-    console.log(student[i]);
+// // console.log(student[0]);
+// // console.log(student[1].marks);
+// for (let i = 0; i < student.length; i++) {
+//   if (student[i].marks > 90) {
+//     console.log(student[i]);
+//   }
+// }
+
+let shows = [
+  { type: "movie", name: "Avengers", rating: 4.5 },
+  { type: "TV Show", name: "TMKO", rating: 4.8 },
+  { type: "movie", name: "Inception", rating: 4.9 },
+  { type: "TV Show", name: "Friends", rating: 4.7 },
+];
+let res = [];
+for (let i = 0; i < shows.length; i++) {
+  if (shows[i].type == "movie") {
+    res.push(shows[i]);
   }
 }
+console.log(res);
